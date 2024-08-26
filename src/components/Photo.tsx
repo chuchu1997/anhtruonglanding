1adba312bfaec7713724ff094f71de095cf778db
Cuong <cuong@foxit.co.jp>
2024-08-26 10:36:35 +0700

    test

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { MotionDiv } from "./MotionDiv";
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
+      <MotionDiv
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+
+          borderRadius: "50%",
+          //   backgroundColor: "#3498db",
+          border: "4px dashed  #63b8eb", // Dashed border with a blue color
+          borderStyle: "dashed double none",
+          color: "#fff",
+          fontSize: "24px",
+          fontWeight: "bold",
+          textAlign: "center",
+          lineHeight: "200px",
+        }}
+        className=" w-[300px] h-[300px]   xl:w-[550px] xl:h-[550px] overflow-hidden"
+        initial={{ strokeDasharray: "24 10 0 0" }}
+        animate={{ strokeDasharray: ["15 120 25 105", "16 25 50 50", "4 250 50 50"], rotate: [120, 360] }}
+        transition={{ duration: 18, repeat: Infinity, repeatType: "reverse" }}
+      >
+        <Image
+          src="/bannerphoto.png"
+          // sizes="(max-width: 768px) 100vw,(max-width:1200px) 50vw,33vw"
+          fill
+          alt=""
+          className="object-contain rounded-full p-2"
+          priority
+          quality={25}
+        />
+      </MotionDiv>
       {/* <motion.div
         style={{
           display: "flex",
@@ -29,18 +61,6 @@ const Photo = () => {
       
       
       </motion.div> */}
-
-      <div className="w-[300px] h-[300px]   xl:w-[550px] xl:h-[550px] overflow-hidden">
-        <Image
-          src="/bannerphoto.png"
-          // sizes="(max-width: 768px) 100vw,(max-width:1200px) 50vw,33vw"
-          fill
-          alt=""
-          className="object-contain rounded-full p-2"
-          priority
-          quality={35}
-        />
-      </div>
     </div>
   );
 };
