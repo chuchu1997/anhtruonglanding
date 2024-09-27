b4b63c66b3f57c24f09873119359521daf919777
Cuong <cuong@foxit.co.jp>
2024-09-27 15:31:57 +0700

    create product

--- src/components/GridLayout.tsx
+++ src/components/GridLayout.tsx
@@ -8,7 +8,7 @@ type Props = {
   children: React.ReactNode;
 };
 const GridLayout: React.FC<Props> = ({ children }) => {
-  return <div className="grid grd-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-8 mt-10 px-10 xl:px-0">{children}</div>;
+  return <div className="grid grd-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-10 px-10 xl:px-0">{children}</div>;
 };
 
 export default GridLayout;
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import Image from "next/image";
 import PhoneCall from "./PhoneCall";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 type Props = {
   id?: string;
@@ -32,23 +33,34 @@ const ProductComponent: React.FC<Props> = ({ id, title, description, price, link
 
     //   {/* <PhoneCall /> */}
     // </div>
-    <Link href={linkHref} className="overflow-hidden relative group p-2 border border-gray rounded-md  group w-full flex flex-col gap-4">
-      <div className="bg-gradient-to-b from-blue-500 rounded-b-md  to-cyan-400   absolute top-0 right-[20px] z-50 overflow-hidden w-[65px] h-[30px] text-[12px] text-center">
+    <div className=" relative group  border border-gray rounded-lg  group w-full flex flex-col gap-4 bg-white">
+      {/* <div className="bg-gradient-to-b from-blue-500 rounded-b-md  to-cyan-400   absolute top-0 right-[20px] z-50 overflow-hidden w-[65px] h-[30px] text-[12px] text-center">
         <div className="pt-[4px] text-white font-semibold">HS</div>
+      </div> */}
+      <div className="w-full h-[50px] bg-[#2b95f3] z-20 relative "></div>
+      <div className="product-content relative bg-white top-[-40px] rounded-tr-[35px] rounded-tl-[35px] z-50">
+        <div className="h-[250px]  relative mx-[15px] mt-[14px] ">
+          <Image
+            src={imageSrc}
+            alt={title}
+            className="object-cover rounded-lg "
+            quality={100}
+            priority
+            fill
+            sizes="(max-width: 768px) 100vw,(max-width:1200px) 50vw,33vw"
+          />
+        </div>
+        <h4 className=" mx-[15px] text-[#2b95f3] cursor-default pt-[4px] font-bold">{title}</h4>
+        <div className="mx-[15px] font-extrabold">$45</div>
       </div>
-      <div className="h-[250px]  relative ">
-        <Image
-          src={imageSrc}
-          alt={title}
-          fill
-          className="object-contain"
-          quality={100}
-          priority
-          sizes="(max-width: 768px) 100vw,(max-width:1200px) 50vw,33vw"
-        />
-      </div>
-      NGUYEN CUONG
-    </Link>
+      <Link
+        href="/"
+        className="absolute bg-[#2b95f3] py-[4px] text-[14px] text-center text-white font-bold rounded-lg z-50 w-5/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bottom-[-32px]"
+      >
+        Xem Sản Phẩm
+      </Link>
+      {/* <div className="h-[50px] w-full bg-[#ddc01b] text-center flex justify-center items-center text-[14px] font-semibold">Xem sản phẩm</div> */}
+    </div>
   );
 };
 
