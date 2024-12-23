b3b80fd08ecc2de6ca97aaeb88a714437105b64a
Cuong <cuong@foxit.co.jp>
2024-12-23 09:14:56 +0700

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -184,9 +184,9 @@ export default function Home() {
 
       <section className="container my-8 h-full mx-auto mt-[40px] xl:mt-[80px] ">
         <DynamicSectionTitle title="Các dự án tiêu biểu" />
-        <p className="font-bold text-center py-4 text-[18px]">
+        {/* <p className="font-bold text-center py-4 text-[18px]">
           Thương Hiệu Cầm Tay: Bosch - Makita - Dong cheng - Dewalt - Ken
-        </p>
+        </p> */}
         <DynamicNews />
       </section>
     </main>
