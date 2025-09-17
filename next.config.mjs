70d5980e66a7471d206892f2653a1293619f74ac
cuong <cuong@foxit.co.jp>
2025-09-17 09:37:58 +0700

    ignore build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     // output:"export",
     reactStrictMode: true,
+    eslint: {
+    ignoreDuringBuilds: true, // 🚀 build không check ESLint
+    },
     images: {
       // path: "/",
       //EDIT PATH /
